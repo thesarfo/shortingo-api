6b69439d91ab17f99498f66e3b19d9a905e86dfc
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-01-05 12:27:38 +0000

    added retrywrites

--- src/server.ts
+++ src/server.ts
@@ -8,25 +8,34 @@ import app from "./app";
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL: string = process.env.MONGO_URL!;
+const RETRY_WRITES = process.env.RETRY_WRITES;
 
 const server: http.Server = http.createServer(app);
 
-mongoose.connection.once('open', ()=>{
-    console.log("MongoDB connection is ready")
+mongoose.connection.once('open', () => {
+  console.log("MongoDB connection is ready");
 });
 mongoose.connection.on("error", (err: Error) => {
-    console.error(err);
+  console.error(err);
 });
 
 async function startServer(): Promise<void> {
-    try{
-        await mongoose.connect(MONGO_URL);
-        server.listen(PORT, () => {
-            console.log(`Listening on port ${PORT}`)
-        })
-    } catch(error: any){
-        console.error("Error starting the server:", error)
+  try {
+    if (!MONGO_URL) {
+      throw new Error("MongoDB URL not in env variable");
     }
+
+    const uriWithRetryWrites = MONGO_URL.includes("retryWrites")
+      ? MONGO_URL
+      : `${MONGO_URL}&retryWrites=${RETRY_WRITES}`;
+
+    await mongoose.connect(uriWithRetryWrites);
+    server.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  } catch (error: any) {
+    console.error("Error starting the server:", error);
+  }
 }
 
 startServer();
