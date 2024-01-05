29ec7e7d2cd6f22d8a8d8ad3d1de0df5b12b95ff
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-01-05 07:24:43 +0000

    added the rud endpoints

--- src/controllers/urlController.ts
+++ src/controllers/urlController.ts
@@ -22,3 +22,63 @@ export const createUrl = async (req: Request, res: Response) => {
         });
     }
 };
+
+export const getUrl = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const url: IURL | null = await urlModel.findById(id);
+        if (!url) {
+            return res.status(404).json({ message: 'URL not found' });
+        }
+        res.status(200).json(url);
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal Server Error',
+            error: error.message,
+        });
+    }
+};
+
+export const updateUrl = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { title, date, shortLink, ogLink, starred }: { title: string; date: Date; shortLink: string; ogLink: string; starred: boolean } = req.body;
+
+    try {
+        const url: IURL | null = await urlModel.findById(id);
+        if (!url) {
+            return res.status(404).json({ message: 'URL not found' });
+        }
+
+        url.title = title;
+        url.date = date;
+        url.shortLink = shortLink;
+        url.ogLink = ogLink;
+        url.starred = starred;
+
+        const updatedUrl: IURL = await url.save();
+        res.status(200).json(updatedUrl);
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal Server Error',
+            error: error.message,
+        });
+    }
+};
+
+export const deleteUrl = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const deletedUrl: IURL | null = await urlModel.findByIdAndDelete(id);
+        if (!deletedUrl) {
+            return res.status(404).json({ message: 'URL not found' });
+        }
+        res.status(200).json({ message: 'URL deleted successfully' });
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal Server Error',
+            error: error.message,
+        });
+    }
+};
