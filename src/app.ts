85abfb156c0c930883020dd7f07169a1871afff1
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-01-06 16:37:19 +0000

    allowed cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import urlRoute from "./routes/urlRoutes";
 import userRoute from "./routes/userRoutes";
@@ -6,6 +7,8 @@ import userRoute from "./routes/userRoutes";
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/api", urlRoute);
 app.use("/api/auth", userRoute);
 
