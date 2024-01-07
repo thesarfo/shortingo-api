1a6e01c84cb50b2b724be0bffc1947c465c65bb8
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-01-07 17:52:24 +0000

    added a search feat, patch feat, and modified some endpoints

--- src/controllers/urlController.ts
+++ src/controllers/urlController.ts
@@ -50,41 +50,23 @@ export const getUserURLs = async (req: Request, res: Response) => {
     }
 };
 
-export const getUrlById = async (req: Request, res: Response) => {
+export const searchUrl = async (req: Request, res: Response) => {
     const urlId = req.params.id;
+    const { title } = req.query;
 
     try {
-        const url: IURL | null = await urlModel.findById(urlId).exec();
+        let query: any = { _id: urlId }; 
+
+        if (title) {
+            query = { title: { $regex: title.toString(), $options: 'i' } }; 
+        }
+
+        const url: IURL | null = await urlModel.findOne(query).exec();
         if (!url) {
             return res.status(404).json({ message: 'URL not found' });
         }
+
         res.status(200).json(url);
-    } catch (error: any) {
-        res.status(500).json({
-            message: 'Internal Server Error',
-            error: error.message,
-        });
-    }
-};
-
-export const updateUrl = async (req: Request, res: Response) => {
-    const urlId = req.params.id;
-    const { title, date, shortLink, ogLink, starred }: IURL = req.body;
-
-    try {
-        const url: IURL | null = await urlModel.findById(urlId).exec();
-        if (!url) {
-            return res.status(404).json({ message: 'URL not found' });
-        }
-
-        url.title = title;
-        url.date = date;
-        url.shortLink = shortLink;
-        url.ogLink = ogLink;
-        url.starred = starred;
-
-        const updatedUrl: IURL = await url.save();
-        res.status(200).json(updatedUrl);
     } catch (error: any) {
         res.status(500).json({
             message: 'Internal Server Error',
@@ -110,3 +92,60 @@ export const deleteUrl = async (req: Request, res: Response) => {
         });
     }
 };
+
+export const patchUrl = async (req: Request, res: Response) => {
+    const { title, shortLink, ogLink, starred, date }: Partial<IURL> = req.body;
+    const urlId = req.params.id;
+  
+    const token = req.headers.authorization?.split(' ')[1];
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized: Token missing' });
+    }
+  
+    try {
+      const decoded: any = jwt.verify(token, process.env.SECRET_KEY || '');
+      const userId = decoded.userId;
+  
+      const url: IURL | null = await urlModel.findById(urlId).exec();
+  
+      if (!url) {
+        return res.status(404).json({ message: 'URL not found' });
+      }
+  
+      if (url.user.toString() !== userId) {
+        console.log(decoded);
+        console.log(url.user);
+        console.log(userId);
+        return res.status(403).json({ message: 'Unauthorized: Cannot update other user\'s URLs' });
+      }
+    //   const updatedUrl = { ...url, ...req.body };
+    //   updatedUrl.user = userId;
+
+    if (title) {
+        url.title = title;
+      }
+      if (shortLink) {
+        url.shortLink = shortLink;
+      }
+      if (ogLink) {
+        url.ogLink = ogLink;
+      }
+      if (starred !== undefined) {
+        url.starred = starred;
+      }
+      if (date) {
+        url.date = date;
+      }
+  
+      url.user = userId;
+  
+      const savedUrl: IURL = await url.save();
+      res.status(200).json(savedUrl);
+    } catch (error: any) {
+      res.status(500).json({
+        message: 'Failed to patch URL',
+        error: error.message,
+      });
+    }
+  };
--- src/routes/urlRoutes.ts
+++ src/routes/urlRoutes.ts
@@ -1,16 +1,16 @@
 import express from "express";
 
 import {
-    createUrl, deleteUrl, getUrlById, getUserURLs, updateUrl
+    createUrl, deleteUrl, searchUrl, getUserURLs, patchUrl
 } from "../controllers/urlController";
 import { verifyToken } from "../middleware/verifyToken";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createUrl); 
-router.get("/getAll/:id", verifyToken, getUserURLs); 
-router.get("/getOne/:id", verifyToken, getUrlById);
-router.put("/update/:id", verifyToken, updateUrl);
+router.get("/get-all/:id", verifyToken, getUserURLs); 
+router.get("/search", verifyToken, searchUrl);
 router.delete("/delete/:id", verifyToken, deleteUrl); 
+router.patch("/update/:id", patchUrl)
 
 export default router;
